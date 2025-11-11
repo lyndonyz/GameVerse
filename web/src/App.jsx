de544b74d374e0c386d9e990656b535251a2566d
Lyndon Zhang <lyndonyz@gmail.com>
2025-11-10 19:39:41 -0500

    smaller changes.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   return (
     <>
       <div>
+        <a href="https://vite.dev" target="_blank">
+        </a>
         <a href="https://react.dev" target="_blank">
           <img src={gameverselogo} className="logo react" alt="GameVerse logo" />
         </a>
