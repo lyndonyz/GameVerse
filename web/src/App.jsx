bcb1a00aa48ac86e09df18ef5a511bbbb33e33a0
Lyndon Zhang <lyndonyz@gmail.com>
2025-11-10 19:38:33 -0500

    Made changes, added temp logo.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import gameverselogo from './assets/logo.png'
 import './App.css'
 
 function App() {
@@ -9,11 +8,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={gameverselogo} className="logo react" alt="GameVerse logo" />
         </a>
       </div>
       <h1>GameVerse</h1>
@@ -26,7 +22,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        FYI we use Vite + React + TailwindCSS.
       </p>
     </>
   )
