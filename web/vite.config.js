815511be50e4764cf3bf2c3a54c6dcefdf341e73
Lyndon Zhang <lyndonyz@gmail.com>
2025-12-03 16:26:47 -0500

    updated vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // Explicitly confirm the desired modern runtime
+      jsxRuntime: 'automatic', 
+    }),
+  ],
   server: {
     proxy: {
       // "/api": "http://localhost:8080",
