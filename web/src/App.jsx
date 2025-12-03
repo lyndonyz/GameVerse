be111961365d3359293cbe495650bd3535f4f91c
Lyndon Zhang <lyndonyz@gmail.com>
2025-12-03 16:34:51 -0500

    reverting

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { useEffect, useMemo, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "./AuthContext.jsx";
