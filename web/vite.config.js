c008d3463bd1b7573893141dcbfcd6da08668272
Lyndon Zhang <lyndonyz@gmail.com>
2025-12-03 16:06:09 -0500

    Frontend fix

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
+      // "/api": "http://localhost:8080",
+      "/api": "https://my-backend-api.23gzti4bhp77.ca-tor.codeengine.appdomain.cloud",
     },
   },
 });
