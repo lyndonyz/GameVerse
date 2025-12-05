8df200897363f3158b729483932b06b69608743b
Lyndon <lyzndon@gmail.com>
2025-12-05 12:53:51 -0500

    Deployment changes

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic', 
-    }),
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": "http://localhost:8000",
       // "/api": "https://my-backend-api.23gzti4bhp77.ca-tor.codeengine.appdomain.cloud",
     },
+  },
+  build: {
+    sourcemap: false,
   },
 });
